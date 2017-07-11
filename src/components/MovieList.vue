c5c5f73d37f8bc45fd2a4953e8f51e622e8a90b2
iMyloMylo <imylomylo@gmail.com>
2017-07-12 02:25:44 +1000

    Filters are working harmoniously after lecture 96

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,10 +1,15 @@
 <template>
     <div id="movie-list">
         <div v-if="filteredMovies.length">
-            <movie-item v-for="movie in filteredMovies" class="movie" v-bind:movie="movie.movie"></movie-item>
+            <movie-item v-for="movie in filteredMovies" class="movie" 
+                    v-bind:movie="movie.movie" 
+                    v-bind:sessions="movie.sessions" 
+                    v-bind:day="day"
+                    v-bind:time="time">
+            </movie-item>
         </div>
         <div v-else-if="movies.length" class="no-results">
-            No results.
+            {{ noResults }}
         </div>
         <div v-else class="no-results">
             Loading ...
@@ -15,11 +20,12 @@
 </template>
 <script>
 import genres from '../util/genres'
+import times from '../util/times'
 import MovieItem from './MovieItem.vue'
 
 
     export default {
-            props: [ 'genre', 'time', 'movies'],
+            props: [ 'genre', 'time', 'movies', 'day'],
             methods: {
                 moviePassesGenreFilter(movie){
                     if( !this.genre.length ){
@@ -34,15 +40,36 @@ import MovieItem from './MovieItem.vue'
                         });
                         return matched ;
                     }
+                },
+                sessionPassesTimeFilter(session){
+                    if( !this.day.isSame(this.$moment(session.time), 'day' )){
+                        return false ;
+                    } else if (this.time.length === 0 || this.time.length === 2 ){
+                        return true ;
+                    } else if ( this.time[0] === times.AFTER_6PM ){
+                        return this.$moment(session.time).hour() >= 18;
+                    } else {
+                        return this.$moment(session.time).hour() < 18;
+                    }
                 }
             },
             computed: {
                 filteredMovies() {
-                    return this.movies.filter(this.moviePassesGenreFilter) ;
+                    return this.movies
+                        .filter(this.moviePassesGenreFilter)
+                        .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+                },
+                noResults() {
+                    let times = this.time.join(', ');
+                    let genres = this.genre.join(', ');
+                    return `No results for ${times}${ times.length && genres.length ? ', ': ' '}${genres}.`;
                 }
             },
             components: {
                 MovieItem
+            },
+            created() {
+                console.log(this.$moment);
             }
         }
 
--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-import Vue from 'vue'
-import './style.scss'
+import Vue from 'vue';
+import './style.scss';
 
-import MovieList from './components/MovieList.vue'
-import MovieFilter from './components/MovieFilter.vue'
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
-import VueResource from 'vue-resource'
+import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
+
+import moment from 'moment-timezone';
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
 new Vue({
     el: '#app',
     data: {
         genre: [],
         time: [],
-        movies: []
+        movies: [],
+        moment,
+        day: moment()
     },
     methods: {
         checkFilter(category, title, checked) {
